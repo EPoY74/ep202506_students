7d6b8282144b415dbac0591fe6a141b47a69ef55
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-08 19:37:25 +0500

    Подготовка docker compose

---
+++ db/init/init-db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Initializing database..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
+    CREATE DATABASE $DB_NAME;
+    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+    \connect $DB_NAME
+
+    GRANT USAGE, CREATE ON SCHEMA public TO $DB_USER;
+    ALTER USER $DB_USER WITH SUPERUSER CREATEDB;
+EOSQL
+
+echo "Database initialized!"
