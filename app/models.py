8090f0ccae75baecc2119a36e16366ddefa640f2
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-09 23:20:58 +0500

    Ruff форматирование

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, ForeignKey, Index, DateTime,func
+from sqlalchemy import String, Integer, ForeignKey, Index, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
@@ -12,12 +12,13 @@ class StudentStatus(Base):
     status_code (str) - Код статуса сткдена (на английском)
     status_label (str) - Метка статуса студента (на русском)
     """
-    __tablename__="student_status"
+
+    __tablename__ = "student_status"
     status_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     status_code: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     status_label: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
 
-    students: Mapped[list['Student']] = relationship("Student", back_populates="status")
+    students: Mapped[list["Student"]] = relationship("Student", back_populates="status")
 
 
 class StudentExtraInfo(Base):
@@ -32,25 +33,26 @@ class StudentExtraInfo(Base):
     created_at (DateTime) - Дата создания дополнительной информации
     updated_at (DateTime) - Дата последноего изменения информации
     """
-    __tablename__= 'student_extra_info'
+
+    __tablename__ = "student_extra_info"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    student_id: Mapped[int] = mapped_column(ForeignKey('students.id'), nullable=False, index=True)
+    student_id: Mapped[int] = mapped_column(
+        ForeignKey("students.id"), nullable=False, index=True
+    )
     info_type: Mapped[str] = mapped_column(String(30), nullable=False, index=True)
-    info_value: Mapped[str] = mapped_column(String, nullable=False)
+    info_value: Mapped[str] = mapped_column(String(255), nullable=False)
 
     created_at: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=False),
-        server_default=func.now(),
-        nullable=False
+        DateTime(timezone=False), server_default=func.now(), nullable=False
     )
     updated_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=False),
         server_default=func.now(),
         onupdate=func.now(),
-        nullable=False
+        nullable=False,
     )
 
-    student: Mapped['Student'] = relationship('Student', back_populates='extra_info')
+    student: Mapped["Student"] = relationship("Student", back_populates="extra_info")
 
 
 class Student(Base):
@@ -64,32 +66,34 @@ class Student(Base):
     first_name (str) - Имя пользователя
     last_name (str) Фамилия пользователя
     email (str) - Электронная почта пользователя (Уникальный)
-    created_at (DateTime) - Дата создания карточки студента 
+    created_at (DateTime) - Дата создания карточки студента
     updated_at (DateTime) - Дата последноего карточки студента
     """
 
     __tablename__ = "students"
     __table_args__ = (
-    Index('ix_student_lastname_firstname', 'last_name', 'first_name'),
+        Index("ix_student_lastname_firstname", "last_name", "first_name"),
     )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     first_name: Mapped[str] = mapped_column(String(50), index=True)
     last_name: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     status_code: Mapped[str] = mapped_column(ForeignKey("student_status.status_code"))
 
     created_at: Mapped[DateTime] = mapped_column(
-        DateTime(timezone=False),
-        server_default=func.now(),
-        nullable=False
+        DateTime(timezone=False), server_default=func.now(), nullable=False
     )
     updated_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=False),
         server_default=func.now(),
         onupdate=func.now(),
-        nullable=False
+        nullable=False,
     )
 
-    status: Mapped["StudentStatus"] = relationship("StudentStatus", back_populates="students")
-    extra_info: Mapped[list['StudentExtraInfo']] = relationship("StudentExtraInfo", back_populates="student")
+    status: Mapped["StudentStatus"] = relationship(
+        "StudentStatus", back_populates="students"
+    )
+    extra_info: Mapped[list["StudentExtraInfo"]] = relationship(
+        "StudentExtraInfo", back_populates="student"
+    )
