62ccd4cd3729b62b37f9d0dedce2df89b2626b59
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-09 21:42:37 +0500

    Поправил наименование моделей данных

--- app/crud.py
+++ app/crud.py
@@ -1,20 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from app.models import User
+from app.models import Student
 from app.schemas import UserCreate
 
 
 async def get_users(db: AsyncSession):
-    result = await db.execute(select(User))
+    result = await db.execute(select(Student))
     return result.scalars().all()
 
 
 async def get_user(db: AsyncSession, user_id: int):
-    return await db.get(User, user_id)
+    return await db.get(Student, user_id)
 
 
 async def create_user(db: AsyncSession, user: UserCreate):
-    db_user = User(**user.model_dump())
+    db_user = Student(**user.model_dump())
     db.add(db_user)
     await db.commit()
     await db.refresh(db_user)
@@ -22,7 +22,7 @@ async def create_user(db: AsyncSession, user: UserCreate):
 
 
 async def delete_user(db: AsyncSession, user_id: int):
-    user = await db.get(User, user_id)
+    user = await db.get(Student, user_id)
     if user:
         await db.delete(user)
         await db.commit()
