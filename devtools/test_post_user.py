ad400053f4f91ce72d6d95eae71fd5594e821e2d
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-10 14:21:47 +0500

    Поменял отпраляемые тестовые данные

--- devtools/test_post_user.py
+++ devtools/test_post_user.py
@@ -2,7 +2,7 @@ import json
 
 import httpx
 
-data = {"name": "eugenii_2", "email": "eugenii_2@eugenii.ru"}
+data = {"name": "eugenii_1", "email": "eugenii_2@eugenii.ru"}
 
 response = httpx.post("http://0.0.0.0:8000/users/", json=data)
 print(response.status_code, response.json())
