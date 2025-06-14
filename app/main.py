9808a30653c19a7b8b7b5ccabb2955077374b1ab
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-14 09:34:59 +0500

    Добавил настройки логгирования (одни на всех), создал обработчик для загрузки начальных данных в таблицу StudentStatus, удалил некоррекный код по инициализации, добавл само логгирование в файл

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ API для работы со студентами
 """
 
 import logging
-import sys
+# import sys
 
 from typing import Any, AsyncGenerator
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import select, func
+from sqlalchemy import select, event
 from sqlalchemy.ext.asyncio import AsyncSession
 from .database import async_session, engine, Base
 from contextlib import asynccontextmanager
@@ -19,17 +19,38 @@ from app.models import StudentStatus
 
 def setup_logging():
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format="[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
-        handlers=[
-            logging.StreamHandler(sys.stdout),
-            logging.FileHandler("app.log", encoding="utf-8"),
-        ],
+        # handlers=[
+        #     logging.StreamHandler(sys.stdout),
+        #     logging.FileHandler("app.log", encoding="utf-8"),
+        # ],
     )
 
 
 setup_logging()
 logger = logging.getLogger(__name__)
+
+# Создаем обработчик события
+def after_create(target, connection, **kw):
+    logger.info("СТАРТ: Запуск инициализации таблицы StudentStatus начальными значениями")
+    if not connection.execute(select(StudentStatus).limit(1)).fetchone():
+        logger.info("НАЧАЛО: Инициализация таблицы StudentStatus")
+        connection.execute(
+            StudentStatus.__table__.insert(),
+            [
+                {"status_code": "active", "status_label": "Обучается"},
+                {"status_code": "academic_leave", "status_label": "Академический отпуск"},
+                {"status_code": "expelled", "status_label": "Отчислен"},
+                {"status_code": "reinstated", "status_label": "Восстановлен"},
+                {"status_code": "graduated", "status_label": "Завершил обучение"},
+                {"status_code": "transferred", "status_label": "Переведён"},
+                {"status_code": "postgraduate", "status_label": "Продолжает обучение"},
+                {"status_code": "debt", "status_label": "Академическая задолженность"},
+            ]
+        )
+        logger.info("КОНЕЦ: Инициализация таблицы StudentStatus")
+        logger.info("ФИНИШ: Запуск инициализации таблицы StudentStatus начальными значениями")
 
 
 @asynccontextmanager
@@ -38,40 +59,19 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
     Создает таблицы при первом запуске
     """
     async with engine.begin() as conn:
+        logger.info('СТАРТ: Инициализация таблиц при первом запуске')
+        # Подписываемся на событие
+        event.listen(StudentStatus.__table__, 'after_create', after_create)
         await conn.run_sync(Base.metadata.create_all)
-        yield
+        logger.info('ФИНИШ: Инициализация таблиц при первом запуске')
+        
 
-    async with AsyncSession(engine) as session:
-        result = await session.execute(select(func.count()).select_from(StudentStatus))
-        student_status_count = result.scalar()
-        logging.info("")
+    yield    
 
-        if student_status_count == 0:
-            student_status = [
-                # StudentStatus(name="Alice", email="alice@example.com"),
-                # StudentStatus(name="Bob", email="bob@example.com")
-                StudentStatus(status_code="active", status_label="Обучается"),
-                StudentStatus(
-                    status_code="academic_leave", status_label="Академический отпуск"
-                ),
-                StudentStatus(status_code="expelled", status_label="Отчислен"),
-                StudentStatus(status_code="reinstated", status_label="Восстановлен"),
-                StudentStatus(
-                    status_code="graduated", status_label="Завершил обучение"
-                ),
-                StudentStatus(status_code="transferred", status_label="Переведён"),
-                StudentStatus(
-                    status_code="postgraduate", status_label="Продолжает обучение"
-                ),
-                StudentStatus(
-                    status_code="debt", status_label="Академическая задолженность"
-                ),
-            ]
-            await session.add_all(student_status)
-            await session.commit()
-
-
+   
+logger.info("Запуск приложения")
 app = FastAPI(lifespan=lifespan)
+logger.info("Приложение запущено")
 
 
 # Зависимость
@@ -79,42 +79,48 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """
     Создает сессию для работы с БД
     """
+
     async with async_session() as session:
+        logger.debug('Инициалзация сесии:')
         yield session
 
 
-@app.post("/users/", response_model=schemas.StudentRead)
+@app.post("/create_user/", response_model=schemas.StudentRead)
 async def create_user(user: schemas.StudentCreate, db: AsyncSession = Depends(get_db)):
     """
     Создает пользователя
     """
+    logger.debug('Обращение post по /create_user/ создание пользователя')
     return await crud.create_user(db, user)
 
 
-@app.get("/users/", response_model=list[schemas.StudentRead])
+@app.get("/read_users/", response_model=list[schemas.StudentRead])
 async def read_users(db: AsyncSession = Depends(get_db)):
     """
     Читает все пользователей
     """
+    logger.debug('Обращение get по /read_users/, все пользователи')
     return await crud.get_users(db)
 
 
-@app.get("/users/{user_id}", response_model=schemas.StudentRead)
+@app.get("/read_user/{user_id}", response_model=schemas.StudentRead)
 async def read_user(user_id: int, db: AsyncSession = Depends(get_db)):
     """
     Читает одного пользователя
     """
+    logger.debug('Чтение данных об одном пользователе по /read_user/')
     user = await crud.get_user(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
-@app.delete("/users/{user_id}")
+@app.delete("/user_delete/{user_id}")
 async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
     """
     Удаляе одного пользователя
     """
+    logger.debug('Обращение по /user_delete/ удаление пользователя')
     success = await crud.delete_user(db, user_id)
     if not success:
         raise HTTPException(status_code=404, detail="User not found")
