15808c538820627c62c322fc8261a7cce6e7647f
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-14 09:39:05 +0500

    Добавляю новые данные в БД

--- devtools/test_post_user.py
+++ devtools/test_post_user.py
@@ -1,14 +1,22 @@
+
+from datetime import datetime
 import json
 
 import httpx
+
+# date_str = "26-11-1980"
+# date_obj = datetime.strptime(date_str, "%d-%m-%Y").date()
 
 data = {
     "first_name": "eugenii_1",
     "last_name": "petrov_1",
     "email": "eugenii_2@eugenii.ru",
-    "status": "activate",
+    "date_of_birth": "1980-11-26",
+    "status_code": "active",
     }
 
-response = httpx.post("http://0.0.0.0:8000/users/", json=data)
+response = httpx.post("http://0.0.0.0:8000/create_user/", json=data)
+print()
 print(response.status_code, response.json())
+print("-" * 30)
 print(json.dumps(response.json(), indent=4, ensure_ascii=False))
