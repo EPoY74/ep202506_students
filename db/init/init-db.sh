1332b7ee2b4d5e97cfbf41c5539187edc77d036c
Eugenii B. Petrov Asus <epoy74@gmail.com>
2025-06-16 22:52:30 +0500

    Перевел сообщения об инициализации на русский.
    Убрал лишнее при инициализации БД (создавал лишнюю БД, что бы Почстгрес не ругался, что у пользователя нет БД)

--- db/init/init-db.sh
+++ db/init/init-db.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Initializing database..."
+echo "Инициализация БД..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
     CREATE DATABASE $DB_NAME;
-    -- для исключения ошибки что БД для пользователя не создана 
-    CREATE DATABASE epoy;
     GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
     \connect $DB_NAME
 
@@ -15,4 +13,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     ALTER USER $DB_USER WITH SUPERUSER CREATEDB;
 EOSQL
 
-echo "Database initialized!"
+echo "ДБ инициализирована!"
